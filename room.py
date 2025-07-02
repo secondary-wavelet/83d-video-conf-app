f5004a9ebf7a4ff6866643fa4d1f50f66996d9b1
secondary-wavelet <f20230353@goa.bits-pilani.ac.in>
2025-07-02 18:39:25 +0000

    wip

--- room.py
+++ room.py
@@ -1,5 +1,16 @@
 from client import Client
 
+class ClientState:
+    def __init__(self, ID, conn):
+        self.ID = ID
+        # self.conn = conn
+        self.is_sending_audio = False
+        self.is_sending_video = False
+    
+    def __init__(self, client: Client):
+        self.ClientState(client.id, client.conn)
+        
+
 class Room:
     """A Room is where a "call" takes place
 
@@ -7,20 +18,18 @@ class Room:
     A Client can be in multiple Rooms at once. A Server can handle multiple Rooms at once
     """
 
-    def __init__(self, clients: set[Client] | None = set()):
-        self.clients = clients
+    def __init__(self):
+        self.clients: dict[str, ClientState] = {}
     
-    def add(self, client: Client):
-        self.clients.add(client)
+    def add(self, clientID: str):
+        self.clients[clientID] = ClientState(clientID)
+
+    def remove(self, clientID: str):
+        del self.clients[clientID]
         # handle sockets
 
-    def remove(self, client: Client):
-        if client in self.clients:
-            self.clients.remove(client)
-        # handle sockets
+    def toggle_audio(self, clientID: str):
+        self.clients[clientID].is_sending_audio = not self.clients[clientID].is_sending_audio
 
-    def toggle_audio(self, client: Client):
-        pass
-
-    def toggle_video(self, client: Client):
-        pass
+    def toggle_video(self, clientID: str):
+        self.clients[clientID].is_sending_video = not self.clients[clientID].is_sending_video
